a1810acc859d7dc59e4277e714b5ecd82f72ba66
devyonghee <devyonghee@gmail.com>
2019-10-08 09:56:36 +0900

    add alias

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -186,9 +186,7 @@ module.exports = function (webpackEnv) {
                 .map(ext => `.${ext}`)
                 .filter(ext => useTypeScript || !ext.includes('ts')),
             alias: {
-                // Support React Native Web
-                // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-                'react-native': 'react-native-web',
+                'src': paths.appSrc,
             },
             plugins: [
                 // Adds support for installing with Plug'n'Play, leading to faster installs and adding
--- main.js
+++ main.js
@@ -37,36 +37,20 @@ function handleSquirrelEvent() {
     switch (squirrelEvent) {
         case '--squirrel-install':
         case '--squirrel-updated':
-            // Optionally do things such as:
-            // - Add your .exe to the PATH
-            // - Write to the registry for things like file associations and
-            //   explorer context menus
-
-            // Install desktop and start menu shortcuts
             spawnUpdate(['--createShortcut', exeName]);
             setTimeout(app.quit, 1000);
             return true;
 
         case '--squirrel-uninstall':
-            // Undo anything you did in the --squirrel-install and
-            // --squirrel-updated handlers
-
-            // Remove desktop and start menu shortcuts
             spawnUpdate(['--removeShortcut', exeName]);
-
             setTimeout(app.quit, 1000);
             return true;
 
         case '--squirrel-obsolete':
-            // This is called on the outgoing version of your app before
-            // we update to the new version - it's the opposite of
-            // --squirrel-updated
-
             app.quit();
             return true;
     }
-};
-
+}
 let mainWindow = null;
 
 const files = glob.sync(path.join(__dirname, 'main/*.js'));
